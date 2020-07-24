8d4e101b8add6111828f6931efb53210cc13f27d
assroll <Christer.Hagen@gmail.com>
2020-07-24 21:21:19 +0200

    Trying to push to database

---
+++ backend/enrolltest.py
@@ -0,0 +1,33 @@
+
+import requests
+import urlopen
+from firebase import firebase
+
+
+# put your keys in the header
+headers = {
+    "Content-Type": "application/json",
+    "app_id": "15fd50b7",
+    "app_key": "bdaf7ffa5e41f3fa8159286192368710"
+}
+#Switch url for what you are doing (Enroll, verify etc)
+url = "http://api.kairos.com/enroll"
+firebase = firebase.FirebaseApplication('https://autoshop-205c2.firebaseio.com/', None)
+
+payload = """
+  {
+    "image":"https://www.eiendomsmegler1.no/fileshare/fileupload/14933/Christer%20Hagen2.jpg?width=659&height=659&upscale=True&crop=True&x=1368&y=0&scale=0,16436991532458908",
+    "subject_id": "Christer",
+    "gallery_name": "MyGallery"
+  }
+"""
+
+
+request = requests.post(url, data=payload, headers=headers)
+
+requestq = print(request.content)
+print(request.content)
+
+result = firebase.post('/users2', requestq, params={'print': 'pretty'})
+print(result)
+
